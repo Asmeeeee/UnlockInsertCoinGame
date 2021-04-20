3c10fbee2c7ef7e487fae1210b5dca743fd00aa1
Enzo Krouri <enzo.krouri@etu.univ-orleans.fr>
2021-04-20 16:54:42 +0200

    Copie des fichiers iut

--- mud.py
+++ mud.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 import mud.game
 import mud.server
-mud.game.GAME = mud.game.Game("iut")
+mud.game.GAME = mud.game.Game("Insert-Coin")
 mud.game.GAME.load()
 mud.game.GAME.start()
